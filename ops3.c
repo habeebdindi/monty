509e47729b02acd97715e2d796bbcb4d5eecca42
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-26 03:59:59 +0000

    Correct mod()

--- ops3.c
+++ ops3.c
@@ -10,7 +10,7 @@ void _mod(stack_t **head, unsigned int line_num)
 {
 	stack_t *top;
 
-	if (!*head || !(*head)->next)
+	if (!*head || !(*head)->next || !head)
 	{
 		dprintf(2, "L%u: can't mod, stack too short\n", line_num);
 		cleanup_exit();
@@ -21,9 +21,8 @@ void _mod(stack_t **head, unsigned int line_num)
 		cleanup_exit();
 	}
 	top = *head;
-	(*head)->next->n += (*head)->n;
+	(*head)->next->n %= (*head)->n;
 	*head = top->next;
-	if (*head)
-		(*head)->prev = NULL;
+	(*head)->prev = NULL;
 	free(top);
 }
